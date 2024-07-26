155f4d9c54987e277485952af4709caa13b8fbe9
StephenZR03 <steveoncaffeine@gmail.com>
2024-07-26 16:44:26 +0100

    integrate game logic and ui

--- script.js
+++ script.js
@@ -1,55 +1,75 @@
 function player(name) {
     let choice;
+    let score = 0;
     const getChoice = () => choice;
-    const makeChoice = (newChoice) => {
+    const makeUserChoice = (newChoice) => {
         choice = newChoice;
     }
-    return { name, getChoice, makeChoice };
-}
-
-function computer(name) {
-    let choice;
-    const getChoice = () => choice;
-    const makeChoice = () => {
+    const makeCompChoice = () => {
         choice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 2)];
     }
-    return { name, getChoice, makeChoice }
+    const getScore = () => score;
+    const incrScore = () => score++;
+    const resetScore = () => score = 0;
+
+    const makeChoice = name === "user" ? makeUserChoice : makeCompChoice;
+
+    return { name, getChoice, makeChoice, getScore, incrScore, resetScore };
 }
 
-const logicHander = (() => {
-    let user = player("Stephen");
-    let comp = computer("Jarvis");
+const game = (() => {
+    let user = player("user");
+    let comp = player("comp");
+    let round = 0;
 
-    const checkWin = (user, comp) => {
-        if (user === 'scissors' && comp === 'paper') return true;
-        if (user === 'paper' && comp === 'rock') return true;
-        if (user === 'rock' && comp === 'scissors') return true;
+    const checkWin = () => {
+        if (user.getChoice() === 'scissors' && comp.getChoice() === 'paper') return true;
+        if (user.getChoice() === 'paper' && comp.getChoice() === 'rock') return true;
+        if (user.getChoice() === 'rock' && comp.getChoice() === 'scissors') return true;
         return false;
     }
 
-    const checkDraw = (user, comp) => {
-        return user === comp;
+    const checkDraw = () => {
+        return user.getChoice() === comp.getChoice();
     }
 
-    const play = () => {
-        while(true) {
-            user.makeChoice(prompt('rock, paper, scissors?'));
-            comp.makeChoice();
-            console.log(`${user.name} played ${user.getChoice()}`);
-            console.log(`${comp.name} played ${comp.getChoice()}`);
-            if (checkWin(user.getChoice(), comp.getChoice())) {
-                console.log('user won');
-                break;
-            } else if (checkDraw(comp.getChoice(), user.getChoice())) {
-                console.log('draw');
-                break;
-            } else {
-                console.log('user lost');
-                break;
-            }
+    const resetScores = () => { }
+
+    const incrRound = () => round++;
+    const getRound = () => round;
+    const resetRound = () => round = 0;
+
+    return { user, comp, checkWin, checkDraw, getRound, resetRound, incrRound };
+})();
+
+(function ui() {
+    const rock = document.querySelector('.rock');
+    const paper = document.querySelector('.paper');
+    const scissors = document.querySelector('.scissors');
+    const userScoreBox = document.querySelector('.user-score');
+    const compScoreBox = document.querySelector('.comp-score');
+    const roundBox = document.querySelector('.round-count');
+    const result = document.querySelector('.result');
+
+    const play = (event) => {
+        game.user.makeChoice(event.target.className);
+        game.comp.makeChoice();
+        result.textContent = `you played ${game.user.getChoice()} and comp played ${game.comp.getChoice()}`;
+        if (game.checkWin()) {
+            game.user.incrScore();
+        } else if (game.checkDraw()) {
+        } else {
+            console.log('user lost');
+            game.comp.incrScore();
         }
-    }
-    play();
+        game.incrRound();
 
-    // return {};
+        userScoreBox.textContent = game.user.getScore();
+        compScoreBox.textContent = game.comp.getScore();
+        roundBox.textContent = game.getRound();
+    }
+
+    rock.addEventListener('click', play);
+    paper.addEventListener('click', play);
+    scissors.addEventListener('click', play);
 })();
